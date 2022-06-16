3639db1ac5cb9eaf25d6a91488d45a14317b2b0c
LiuFeng <31612033+imneov@users.noreply.github.com>
2022-06-16 10:39:35 +0800

    Feat/add markdown link (#62)
    * feat: add markdown
    
    * feat: update template
    
    * feat: add version

--- tool/cmd/artisan/main.go
+++ tool/cmd/artisan/main.go
@@ -1,26 +1,33 @@
 package main
 
 import (
+	"github.com/tkeel-io/tkeel-interface/tool/cmd/artisan/version"
 	"log"
 
 	"github.com/spf13/cobra"
 	"github.com/tkeel-io/tkeel-interface/tool/cmd/artisan/project"
 	"github.com/tkeel-io/tkeel-interface/tool/cmd/artisan/proto"
 	"github.com/tkeel-io/tkeel-interface/tool/cmd/artisan/render"
+<<<<<<< feat/add-markdown-link
+=======
 	"github.com/tkeel-io/tkeel-interface/tool/pkg/version"
+>>>>>>> main
 )
 
 var rootCmd = &cobra.Command{
 	Use:     "tkeel-tool",
 	Short:   "An elegant toolkit for Go microservices.",
 	Long:    `An elegant toolkit for Go microservices.`,
-	Version: version.Version,
 }
 
 func init() {
 	rootCmd.AddCommand(project.CmdNew)
 	rootCmd.AddCommand(proto.CmdProto)
 	rootCmd.AddCommand(render.CmdMarkdown)
+<<<<<<< feat/add-markdown-link
+	rootCmd.AddCommand(version.VersionCmd)
+=======
+>>>>>>> main
 }
 
 func main() {
