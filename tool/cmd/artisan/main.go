fe883549f2453c916af99afe59aa4f0bbc63d132
LiuFeng <31612033+imneov@users.noreply.github.com>
2022-06-16 15:08:28 +0800

    feat: merge (#64)

--- tool/cmd/artisan/main.go
+++ tool/cmd/artisan/main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"github.com/tkeel-io/tkeel-interface/tool/cmd/artisan/version"
 	"log"
 
 	"github.com/spf13/cobra"
 	"github.com/tkeel-io/tkeel-interface/tool/cmd/artisan/project"
 	"github.com/tkeel-io/tkeel-interface/tool/cmd/artisan/proto"
 	"github.com/tkeel-io/tkeel-interface/tool/cmd/artisan/render"
-<<<<<<< feat/add-markdown-link
-=======
-	"github.com/tkeel-io/tkeel-interface/tool/pkg/version"
->>>>>>> main
+	"github.com/tkeel-io/tkeel-interface/tool/cmd/artisan/version"
 )
 
 var rootCmd = &cobra.Command{
@@ -24,10 +20,7 @@ func init() {
 	rootCmd.AddCommand(project.CmdNew)
 	rootCmd.AddCommand(proto.CmdProto)
 	rootCmd.AddCommand(render.CmdMarkdown)
-<<<<<<< feat/add-markdown-link
 	rootCmd.AddCommand(version.VersionCmd)
-=======
->>>>>>> main
 }
 
 func main() {
